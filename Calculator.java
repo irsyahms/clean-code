2fcc3e7590968a4c05dc5fc5bff18111cc049182
WhiteHoot <laitnisarby@gmail.com>
2021-09-28 15:22:35 +0700

    Refactored Calculator class using Clean Code principle

--- Calculator.java
+++ Calculator.java
@@ -1,57 +1,85 @@
+package com.cleancode.calc;
+
 import java.util.*;
 
 class Calculator {
+
+    private static String userInput;
+    private static List<String> operatorList = new ArrayList<String>();
+    private static double result = 0;
+
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
         System.out.print("Input number and operators that you want to calculate: ");
 
-        try {
-            String nextLine = in.next();
-            List<String> nuOps= new ArrayList<String>();
-
-            //parsing input menjadi ArrayList berdasarkan angka dan operator
-            String temp = "";
-            for (int i = 0; i < nextLine.length(); i++) {
-                if(nextLine.charAt(i) == '+' || nextLine.charAt(i) == '-' || nextLine.charAt(i) == '/') {
-                    nuOps.add(temp);  
-                    nuOps.add(String.valueOf(nextLine.charAt(i)));
-
-                    temp = "";
-                } else {
-                    temp = temp + String.valueOf(nextLine.charAt(i));
-                }
-
-                if (i == nextLine.length() - 1) {
-                    nuOps.add(temp);
-                    temp = "";
-                }
-            }
-            
-            //perhitungan
-            double t = 0;
-            for (int i = 0; i < nuOps.size(); i++) {
-                switch (nuOps.get(i)) {
-                    case "+":
-                        i++;
-                        t = t + Double.parseDouble(nuOps.get(i));
-                        break;
-                    case "-":
-                        i++;
-                        t = t - Double.parseDouble(nuOps.get(i));
-                        break;
-                    case "/":
-                        i++;
-                        t = t / Double.parseDouble(nuOps.get(i));
-                        break;
-                    default:
-                        t = Double.parseDouble(nuOps.get(i));
-                }
-            }
-
-            System.out.println("** Result: " + t);
-
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input");
+        try{
+            userInput = sc.next();
+            parsingInput();
+            System.out.println("** Result: " + calculateNumbers());
+        }catch (Exception e){
+            System.err.println(e);
         }
+    }
+
+    private static void parsingInput() {
+
+        String temp = "";
+        for (int i = 0; i < userInput.length(); i++) {
+            if(userInput.charAt(i) == '+' || userInput.charAt(i) == '-'
+                    || userInput.charAt(i) == '/' || userInput.charAt(i) == '*') {
+
+                operatorList.add(temp);
+                operatorList.add(String.valueOf(userInput.charAt(i)));
+
+                temp = "";
+            } else {
+                temp = temp + String.valueOf(userInput.charAt(i));
+            }
+
+            if (i == userInput.length() - 1) {
+                operatorList.add(temp);
+                temp = "";
+            }
+        }
+    }
+
+    private static double calculateNumbers() {
+
+        for (int i = 0; i < operatorList.size(); i++) {
+
+            if(operatorList.get(i).equalsIgnoreCase("+")){
+                i++;
+                addingNumbers(i);
+            } else if(operatorList.get(i).equalsIgnoreCase("-")){
+                i++;
+                substracingNumbers(i);
+            } else if(operatorList.get(i).equalsIgnoreCase("/")){
+                i++;
+                dividingNumbers(i);
+            } else if(operatorList.get(i).equalsIgnoreCase("*")){
+                i++;
+                multiplyingNumbers(i);
+            } else {
+                result = Double.parseDouble(operatorList.get(i));
+            }
+        }
+        return result;
+    }
+
+    private static void multiplyingNumbers(int index) {
+        result *= Double.parseDouble(operatorList.get(index));
+    }
+
+    private static void dividingNumbers(int index) {
+        result /= Double.parseDouble(operatorList.get(index));
+    }
+
+    private static void substracingNumbers(int index) {
+        result -= Double.parseDouble(operatorList.get(index));
+    }
+
+    private static void addingNumbers(int index) {
+        result += Double.parseDouble(operatorList.get(index));
     }
 }
